eec12c5590739ccbc5fede1b9060ee920d04f70d
OmarNeira <omarneira1@hotmail.com>
2025-01-20 18:30:37 -0500

    Actualizado dashboard.py
    Se organizo un poco la estructura de dashboard para que sea mas facil de entender.

--- src/dashboard.py
+++ src/dashboard.py
@@ -44,24 +44,24 @@ def graficarBarrasAgrupadas(brands, low_prices, high_prices, tooltips_low, toolt
 
     return p
 
-def main():
-    #Objeto tipo db
-    dbController = dbs.db()
+def pregunta_1(dbController):
     df_filtered_1 = dbController.dbPreguntar("pregunta_1")
-    print("Llego a MAIN ESTO:")
-    print(df_filtered_1)
+    #print("Llego a MAIN ESTO:")
+    #print(df_filtered_1)
     # Preparar datos para Bokeh
     df_filtered_1['x'] = df_filtered_1['tienda_name']
     df_filtered_1 = df_filtered_1.dropna(subset=['x', 'precio'])  # Ensure no null values in x and precio
 
     tooltips_1 = (df_filtered_1['producto_name']+" / "+df_filtered_1['precio'].astype(str)).tolist()
     p1 = graficarBarras(df_filtered_1['x'].tolist(), df_filtered_1['precio'].tolist(), "Pregunta 1", tooltips_1)
+    return p1
 
+def pregunta_3(dbController):
     df_filtered_3 = dbController.dbPreguntar("pregunta_3")
-    print("EJE X")
-    print(df_filtered_3['marca'].tolist())
-    print("EJE Y")
-    print(df_filtered_3['rango_precio'].tolist())
+    #print("EJE X")
+    #print(df_filtered_3['marca'].tolist())
+    #print("EJE Y")
+    #print(df_filtered_3['rango_precio'].tolist())
 
     brands = df_filtered_3['marca'].tolist()
     low_prices = df_filtered_3['rango_bajo'].tolist()
@@ -70,16 +70,30 @@ def main():
     tooltips_high = ("Precio más alto: " + df_filtered_3['rango_alto'].astype(str)).tolist()
 
     p3 = graficarBarrasAgrupadas(brands, low_prices, high_prices, tooltips_low, tooltips_high, "Pregunta 3")
+    return p3
 
+def pregunta_4(dbController):
     df_filtered_4 = dbController.dbPreguntar("pregunta_4")
-    print("EJE X")
-    print(df_filtered_4['producto_name'].tolist())
-    print("EJE Y")
-    print(df_filtered_4['precio'].tolist())
+    #print("EJE X")
+    #print(df_filtered_4['producto_name'].tolist())
+    #print("EJE Y")
+    #print(df_filtered_4['precio'].tolist())
 
     tooltips_4 = (df_filtered_4['producto_name'] + " / Stock: " + df_filtered_4['cantidad_disponible'].astype(str) +" / "+ df_filtered_4['precio'].astype(str)).tolist()
     p4 = graficarBarras(df_filtered_4['producto_name'].tolist(), df_filtered_4['precio'].tolist(), "Pregunta 4", tooltips_4)
+    return p4
 
+
+def main():
+    #Objeto tipo db
+    dbController = dbs.db()
+    
+    p1 = pregunta_1(dbController)
+
+    p3 = pregunta_3(dbController)
+
+    p4 = pregunta_4(dbController)
+    
     # Crear descripciones
     descripcion1 = Div(text="<h3>Pregunta 1</h3><p>¿Cuáles son las tiendas con mayores ventas y mejores categorías de vendedor, cuáles son sus productos con más unidades disponibles en la categoría de computación?</p>")
     descripcion3 = Div(text="<h3>Pregunta 3</h3><p>¿Cuáles son los rangos de precios más frecuentes para laptops, agrupados por marca (5 marcas más caras)?</p>")
